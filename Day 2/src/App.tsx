644d7989ec0849b7b4f6329ecb2862afca00675a
Ziyad <mziyadc@gmail.com>
2024-07-12 19:29:15 +0530

    timeline stepper styling done

--- Day 2/src/App.tsx
+++ Day 2/src/App.tsx
@@ -5,16 +5,19 @@ function App() {
     <>
       <div className="progress-container">
         <div className="progress">
+          <div className="progress-line"></div>
           <div className="circle active">1</div>
           <div className="circle">2</div>
           <div className="circle">3</div>
           <div className="circle">4</div>
         </div>
-        <div className="btn" id="prev">
-          Prev
-        </div>
-        <div className="btn" id="next">
-          Next
+        <div className="btn-box">
+          <button className="btn" id="prev" disabled>
+            Prev
+          </button>
+          <button className="btn" id="next">
+            Next
+          </button>
         </div>
       </div>
     </>
