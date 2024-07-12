dd8997a83dfaa875cfe23bd681c774578a1b2bfc
Ziyad <mziyadc@gmail.com>
2024-07-12 18:29:55 +0530

    Day2 vite added

---
+++ Day 2/src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="progress-container">
+        <div className="progress">
+          <div className="circle active">1</div>
+          <div className="circle">2</div>
+          <div className="circle">3</div>
+          <div className="circle">4</div>
+        </div>
+        <div className="btn" id="prev">
+          Prev
+        </div>
+        <div className="btn" id="next">
+          Next
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
